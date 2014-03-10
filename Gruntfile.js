d6b041e68745da610f374f7f829c2630111912af
root <root@ubuntu.(none)>
2014-03-10 00:24:58 -0600

    Commit Inicial Base con Seguridad casi funcional

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			dev: {
+				options: {
+					beautify: true
+				},
+				files: {
+					"public/js/app.js": [ 'js-src/**/*.js' ]
+				}
+			},
+			dist: {
+				files: {
+					"public/js/app.min.js": [ 'js-src/**/*.js' ]
+				}
+			},
+			vendor: {
+				files: {
+					"public/js/vendor.js": [
+						'bower_components/jquery/jquery.js',
+						'bower_components/ng-file-upload/angular-file-upload-shim.min.js',
+						'bower_components/angular/angular.js',
+                        'bower_components/angular-resource/angular-resource.js',
+                        'bower_components/angular-route/angular-route.js',
+                        'bower_components/ng-file-upload/angular-file-upload.min.js',
+						 'bower_components/bootstrap/dist/js/bootstrap.js', 
+					]
+				}
+			}
+		},
+		less: {
+			options: {
+				yuicompress: true	
+			},
+			vendor: {
+				files: {
+					"public/css/vendor.css": [
+						'bower_components/bootstrap/less/bootstrap.less', 
+					]
+
+				}
+			},
+			deploy: {
+				files: {
+					"public/css/styles.css": ['less-src/**/*.less']
+
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	grunt.registerTask('default', ['uglify','less']);
+	grunt.registerTask('vendor', ['uglify:vendor','less:vendor']);
+}
